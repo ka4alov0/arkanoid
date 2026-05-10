989461f1377c44f664518ee6f73a680032cba13e
ka4alov0 <sinotov.egor@yandex.ru>
2026-05-11 01:21:59 +0300

    реализовал вывод шара

--- script.js
+++ script.js
@@ -9,13 +9,30 @@ const platform = {
     speed: 6,
 }
 
+const ball = {
+    x: game.width/2,
+    y: game.height/2 + 80,
+    radius: 10,
+    dx: 2,
+    dy: 2,
+}
+
 function drawPlatform() {
     ctx.fillStyle = 'black'
     ctx.fillRect(platform.x, platform.y, platform.width, platform.height)
 }
 
+function drawBall() {
+    ctx.fillStyle = 'black'
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2)
+    ctx.fill()
+    ctx.closePath()
+}
+
 function draw() {
     drawPlatform()
+    drawBall()
 }
 
 draw()
